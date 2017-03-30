386c25139c468ae1da5155b32e261599afbe1aa5
mf <myemail@isawesome.com>
2017-03-29 20:52:02 -0500

    started drafting gui

---
+++ src/com/mark/TicketManagerGUI.java
@@ -0,0 +1,18 @@
+package com.mark;
+
+import javax.swing.*;
+
+/**
+ * Created by hl4350hb on 3/29/2017.
+ */
+public class TicketManagerGUI {
+    private JPanel rootPanel;
+    private JList openTicketsList;
+    private JButton newTicketButton;
+    private JButton closeTicketButton;
+    private JTextField idTextField;
+    private JTextField descriptionTextField;
+    private JComboBox priorityComboBox;
+    private JTextField reportedByTextField;
+    private JTextField openDateTextField;
+}
